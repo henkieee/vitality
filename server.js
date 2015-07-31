2101975c0f53b12f780799f621fce7a114abea75
henk <henkiexxx4@gmail.com>
2015-07-31 13:51:33 +0200

    making app deploy ready

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     	app = express();
 
+app.set('port', process.env.PORT || 4000);
+
 app.get('/', function (req, res)
 {
   res.sendFile(__dirname + '/index.html');
@@ -12,4 +14,6 @@ app.use(function (req, res)
 	res.sendFile(__dirname + req.url);
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
